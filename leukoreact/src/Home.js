5ab253fd4b30ef1374e42b46922c370044759c6b
leukobeats <rafalmysterio@wp.pl>
2021-06-05 00:51:26 +0200

    Tutorial

--- leukoreact/src/Home.js
+++ leukoreact/src/Home.js
@@ -1,19 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-        { title: "Your new website", body: "lorem ipsum...", author: "pedal", id: 2 },
-        { title: "Pedal to cwel", body: "lorem ipsum...", author: "mario", id: 3 }
-    ]);
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs);
-    }
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() =>{
+        fetch('http://localhost:8000/blogs')
+        .then(res =>{
+            if(!res.ok){
+                throw Error('Couldnt fetach the data for that source');
+            }
+            return res.json();
+        })
+        .then(data =>{
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
+
+        })
+        .catch(err =>{
+            setIsPending(false);
+            setError(err.message);
+        })
+    }, [])
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+            { error && <div>{ error }</div>}
+            { isPending && <div>Loading...</div> }
+            {blogs && <BlogList blogs={blogs} title="All blogs"/>}
         </div>
 
     );
